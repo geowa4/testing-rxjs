37517599dffcca3408375b5ab9e6f5d11430586a
George Adams, IV <g.w.adams.iv@gmail.com>
2016-02-03 15:13:26 -0500

    wrote a couple of tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
-var expect = require('chai').expect
+const expect = require('chai').expect
+const Rx = require('rx')
 
 // Quick intro to Mocha and Chai
 describe('Array', function() {
@@ -8,3 +9,72 @@ describe('Array', function() {
     })
   })
 })
+
+// Test Scheduler
+describe('Sending a single message', function () {
+  const onNext = Rx.ReactiveTest.onNext
+  const onCompleted = Rx.ReactiveTest.onCompleted
+  const comparer = Rx.internals.isEqual
+
+  describe('with disposal before completion', function () {
+    it('should receive one message', function () {
+      const scheduler = new Rx.TestScheduler()
+
+      const input = scheduler.createHotObservable(
+        onNext(100, "foo"),
+        onNext(110, 7),
+        onCompleted(200)
+      )
+
+      const results = scheduler.startScheduler(
+        function () {
+          // Return a pipeline
+          return input.
+            filter(function (x) { return !isNaN(x) }).
+            map(function (x) { return x * 2 })
+        },
+        {
+          created: 50,
+          subscribed: 51,
+          disposed: 150
+        }
+      )
+
+      expect(results.messages.length).to.equal(1)
+      expect(comparer(results.messages[0], onNext(100, 14))).to.be.false
+      expect(comparer(results.messages[0], onNext(110, 14))).to.be.true
+    })
+  })
+
+  describe('with disposal after completion', function () {
+    it('should receive two messages', function () {
+      const scheduler = new Rx.TestScheduler()
+
+      const input = scheduler.createHotObservable(
+        onNext(100, "foo"),
+        onNext(110, 7),
+        onCompleted(200)
+      )
+
+      const results = scheduler.startScheduler(
+        function () {
+          // Return a pipeline
+          return input.
+            filter(function (x) { return !isNaN(x) }).
+            map(function (x) { return x * 2 })
+        },
+        {
+          created: 50,
+          subscribed: 51,
+          disposed: 250
+        }
+      )
+
+      expect(results.messages.length).to.equal(2)
+      expect(comparer(results.messages[0], onNext(100, 14))).to.be.false
+      expect(comparer(results.messages[0], onNext(110, 14))).to.be.true
+      expect(comparer(results.messages[1], onCompleted(200))).to.be.true
+    })
+  })
+})
+
